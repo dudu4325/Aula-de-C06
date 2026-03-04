7da1544732103eab95a49318dfb74c8c74ba0f07
Luis Eduardo <luiseduardomarques60@gmail.com>
2026-03-04 11:32:41 -0300

    Add files via upload

---
+++ Aula03 - exercicio1/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        float lanches1 = 10;
+        float lanches2 = 4;
+        float lanches3 = 2;
+
+        float media = (lanches1 + lanches2 + lanches3)/3;
+
+        System.out.println("Média de lanches: " + media);
+    }
+}
